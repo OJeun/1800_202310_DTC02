6aa6bee75d10015ae17546dfe5aaafd5c4f7fec3
Jeongeun <ohjeoung5224@gmail.com>
2023-03-03 10:31:37 -0800

    Remove firebaseAPI from gitignore file

---
+++ src/firebaseAPI_TEAM02.js
@@ -0,0 +1,18 @@
+//----------------------------------------
+//  Your web app's Firebase configuration
+//----------------------------------------
+var firebaseConfig = {
+    apiKey: "AIzaSyAm6CxtFR8Y8MK-set8QKae7HEV5agcj0E",
+    authDomain: "comp1800-hotdog.firebaseapp.com",
+    projectId: "comp1800-hotdog",
+    storageBucket: "comp1800-hotdog.appspot.com",
+    messagingSenderId: "984374057153",
+    appId: "1:984374057153:web:62c8d3b98c50b23253dc40"
+};
+
+//--------------------------------------------
+// initialize the Firebase app
+// initialize Firestore database if using it
+//--------------------------------------------
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
