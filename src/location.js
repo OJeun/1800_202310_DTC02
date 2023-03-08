0c5d39a272b37bb1a0c3fa27a56f42f2bf4db49e
Jeongeun <ohjeoung5224@gmail.com>
2023-03-08 12:48:35 -0800

    Get long,lat and temperature

---
+++ src/location.js
@@ -0,0 +1,31 @@
+const findMyLocation = () => {
+
+    const success = (position) => {
+        console.log(position)
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        console.log(latitude + " " + longitude);
+
+        const API = '242606ab0ab1a8970d797a2227fb42a7';
+        const units = 'metric';
+        const tempAPIUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid={242606ab0ab1a8970d797a2227fb42a7}&units=${units}`
+        
+        fetch(tempAPIUrl)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+        })
+    }
+
+    const error = () => {
+        console.log("Unable to retrieve your location")
+    }
+
+
+
+    navigator.geolocation.getCurrentPosition(success, error);
+
+}
+
+$( document ).ready(findMyLocation)
+// document.querySelector("#temerature").addEventListener('click', findMyLocation)
