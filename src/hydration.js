747f96c1466aa7f466c5503368201f2870fcddb2
2-Chengs <ivan.cheng011099@gmail.com>
2023-03-09 22:36:23 -0800

    made the timer real nice

--- src/hydration.js
+++ src/hydration.js
@@ -1,7 +1,10 @@
+var timerID;
+
 function timer() {
     var reminderCard = document.getElementById("reminder");
     var activeButton = document.getElementById("submitButton")
     var cancelButton = document.getElementById("cancelButton");
+    var countdown = document.getElementById("countdown");
     var hours = document.getElementById("hours");
     var hoursValue = parseInt(hours.value);
 
@@ -10,16 +13,31 @@ function timer() {
     var minutesValue = parseInt(minutes.value);
 
 
-    var timeInMilliSeconds = hoursValue * 3600 + minutesValue * 60 * 1000
-    console.log(timeInMilliSeconds);
+    var timeInMilliSeconds = hoursValue * 3600 * 1000 + minutesValue * 60 * 1000
+    var endTime = new Date().getTime() + timeInMilliSeconds;
+  
     
     reminderCard.style.display = "none";
     activeButton.style.display = "none";
+    countdown.style.display = "block";
     cancelButton.style.display = "block";
+    
+    timerId = setInterval(() => {
+        var now = new Date().getTime();
+        var timerCountdown = endTime - now;
+        var hoursRemaining = Math.floor(timerCountdown / (1000 * 60 * 60));
+        var minutesRemaining = Math.floor((timerCountdown % (1000 * 60 * 60)) / (1000 * 60));
+        var secondsRemaining = Math.floor((timerCountdown % (1000 * 60)) / 1000);
+        console.log(timerCountdown);
+        //Update the countdown text
+        countdown.textContent = `Drink water in ${hoursRemaining} hours, ${minutesRemaining} minutes, and ${secondsRemaining} seconds`;
 
-    setTimeout(() =>{
-        alert("times up");
-    }, timeInMilliSeconds);
+        if (timerCountdown < 0) {
+            clearInterval(timerId);
+            countdown.textContent = "Time to Drink Water!"
+        }        
+    }, 1000);
+
     
 }
 
@@ -27,10 +45,13 @@ function cancelTimer() {
     var reminderCard = document.getElementById("reminder");
     var activeButton = document.getElementById("submitButton")
     var cancelButton = document.getElementById("cancelButton");
+    var countdown = document.getElementById("countdown");
 
     reminderCard.style.display = "block";
     activeButton.style.display = "block";
     cancelButton.style.display = "none";
+    countdown.style.display = "none";
+    clearInterval(timerId);
 
 }
 
