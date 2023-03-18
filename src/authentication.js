7941434e5aeaf8f0aedb28def114e41aec9401fc
2-Chengs <ivan.cheng011099@gmail.com>
2023-03-18 01:51:19 -0700

    updated form

--- src/authentication.js
+++ src/authentication.js
@@ -1,9 +1,13 @@
+
+
+
 // Initialize the FirebaseUI Widget using Firebase.
+
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
 var uiConfig = {
     callbacks: {
-        signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+        signInSuccessWithAuthResult: (authResult, redirectUrl) => {
          // User successfully signed in.
          // Return type determines whether we continue the redirect automatically
          // or whether we leave that to developer to handle.
@@ -21,7 +25,9 @@ var uiConfig = {
                     name: user.displayName,                    //"users" collection
                     email: user.email,                         //with authenticated user's ID (user.uid)
                     country: "Canada",                      //optional default profile info      
-										school: "BCIT"                          //optional default profile info
+										school: "BCIT",                          //optional default profile info
+                    dogs: ""
+                   
              }).then(function () {
                     console.log("New user added to firestore");
                     window.location.assign("main.html");       //re-direct to main.html after signup
