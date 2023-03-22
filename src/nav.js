cef8df70cae8c8809a964e73fb96a273d33836eb
2-Chengs <ivan.cheng011099@gmail.com>
2023-03-22 00:22:58 -0700

    implemented the listing of dogs

--- src/nav.js
+++ src/nav.js
@@ -1,23 +1,34 @@
-console.log("hi")
+
+var dogsArray = []
 // user = firebase.auth().currentUser;
 // var dogsCollection = db.collection("users").doc(user.uid).collection("dogs")
 function getName() {
     var user = firebase.auth().currentUser;
-var dogsCollection = db.collection("users").doc(user.uid).collection("dogs");
+    var dogsCollection = db.collection("users").doc(user.uid).collection("dogs");
 
-dogsCollection.get().then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-        // Access the fields in the document using the data() method
-        const dogData = doc.data();
+    dogsCollection.get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            // Access the fields in the document using the data() method
+            const dogData = doc.data();
+            const dogId = doc.id;
 
-        // Log the fields to the console
-        console.log("Dog name:", dogData.name);
-        console.log("Dog breed:", dogData.breed);
-        console.log("Dog age:", dogData.age);
+            dogsArray.push({
+                id: dogId,
+                name: dogData.name,
+                age: dogData.age,
+                breed: dogData.breed
+            })
+
+            // Log the fields to the console
+            // console.log("Dog name:", dogData.name);
+            // console.log("Dog breed:", dogData.breed);
+            // console.log("Dog age:", dogData.age);
+            // console.log(dogId);
+        });
+    }).catch((error) => {        
+        console.log("Error getting dogs collection:", error);
     });
-}).catch((error) => {
-    console.log("Error getting dogs collection:", error);
-});
+    console.log(dogsArray);
 
 }
 
---
+++ src/switchDog.js
@@ -0,0 +1,24 @@
+var currentDog;
+var currentDogId;
+function switchDog() {
+    currentDogId = event.target.getAttribute("data-dog-id");
+    console.log(currentDogId);
+    localStorage.setItem("currentDogId", currentDogId)
+    console.log(localStorage.getItem("currentDogId"))
+}
+// function switchDog() {
+//     console.log("fuckthisshit")
+//     document.querySelectorAll('a[data-dog-id]').forEach((dogLink) => {
+//         dogLink.addEventListener("click", (event) => {
+//         // Prevent the link from navigating to a new page
+//         event.preventDefault();
+        
+//         // Retrieve the ID of the dog from the data attribute
+//         var dogId = event.target.getAttribute("data-dog-id");
+        
+//         // Do something with the ID of the dog
+//         console.log("Clicked dog with ID " + dogId);
+//         });
+//         localStorage.setItem("currentDogId", currentDogId)
+//     });
+// }
