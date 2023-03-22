9e86d7828ceefc944d581a0b2a096f8f27bfda53
Jeongeun <ohjeoung5224@gmail.com>
2023-03-21 18:02:41 -0700

    Take info from addDog

--- src/dogProfile.js
+++ src/dogProfile.js
@@ -20,18 +20,28 @@ doAll();*/
 function addDog() {
 
     console.log('hello')
- 
-     user = firebase.auth().currentUser;
-     var dogsCollection = db.collection("users").doc(user.uid).collection("dogs")
-     dogsCollection.add({
-         name : document.getElementById("nameInput"),
-         breed: "shit zu"
-     }).then((docRef) => {
-         console.log("Document written with ID: ", docRef.id);
-     }).catch((error) => {
-         console.log("Error adding document: ", error);
-     });
-    
+    const dogTakeName = document.getElementById("nameInput");
+    const dogName = nameInput.value;
+    const dogTakeAge = document.getElementById("ageInput");
+    const dogAge = ageInput.value;
+    const dogTakeBreed = document.getElementById("breedInput");
+    const dogBreed = breedInput.value;
+    const dogTakeHair = document.getElementById("hairInput");
+    const dogHair = hairInput.value;
+
+    user = firebase.auth().currentUser;
+    var dogsCollection = db.collection("users").doc(user.uid).collection("dogs")
+    dogsCollection.add({
+        name: dogName,
+        age: dogAge,
+        hair: dogHair,
+        breed: dogBreed
+    }).then((docRef) => {
+        console.log("Document written with ID: ", docRef.id);
+    }).catch((error) => {
+        console.log("Error adding document: ", error);
+    });
+
     // db.collection("users").doc(user.uid).collection("dogs").doc()
     //     .then(userDoc => {
     //         console.log(userDoc.data().name)
@@ -41,7 +51,7 @@ function addDog() {
     //         console.log(doc.data().name)
     //     })
     // })
-    
+
     // if (user) {
     //     console.log("hello world");
     // } else {
