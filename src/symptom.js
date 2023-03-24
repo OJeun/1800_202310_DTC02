71d332e5251c6d1ee2f9c4575665d0484243bee2
MARTIN SIU <msiu28@my.bcit.ca>
2023-03-24 10:55:39 -0700

    readded symptoms page after we reverted the repo

--- src/symptom.js
+++ src/symptom.js
@@ -1,44 +1,112 @@
-var symptoms = [];
+// var symptoms = [];
+// var currentUser;
+
+// function saveUserInfo() {
+//   // get user entered values
+//   var vomiting = document.getElementById('vomiting');
+//   if (vomiting.checked) {
+//     symptoms.push("vomiting");
+//   }
+//   var muscle_spasms = document.getElementById('muscle_spasms');
+//   if (muscle_spasms.checked) {
+//     symptoms.push("muscle_spasms");
+//   }
+//   var dizziness = document.getElementById('dizziness');
+//   if (dizziness.checked) {
+//     symptoms.push("dizziness");
+//   }
+//   var fatigue = document.getElementById('fatigue');
+//   if (fatigue.checked) {
+//     symptoms.push("fatigue");
+//   }
+//   var diarrhea = document.getElementById('diarrhea');
+//   if (diarrhea.checked) {
+//     symptoms.push("diarrhea");
+//   }
+//   var skin_elasticity = document.getElementById('skin_elasticity');
+//   if (skin_elasticity.checked) {
+//     symptoms.push("skin_elasticity");
+//   }
+
+//   // log symptoms for debugging
+//   console.log(symptoms);
+
+//   if (firebase.auth().currentUser) {
+//     console.log('User is signed in');
+//   } else {
+//     console.log('No user is signed in');
+//   }
+
+
+//   firebase.auth().onAuthStateChanged(user => {
+//         // Check if user is signed in:
+//         if (user) {
+//           currentUser = db.collection("users").doc(user.uid)
+//           // update user's symptoms in database
+//           currentUser.update({
+//               symptoms: symptoms,
+//             })
+//             .then(() => {
+//               console.log("Document successfully updated!");
+//             })
+//             .catch((error) => {
+//               console.error("Error updating document: ", error);
+//             });
+//         }
+//       });
+//     }
+
+var currentUser;
 
 function saveUserInfo() {
-  var currentUser = firebase.firestore().collection("users").doc(firebase.auth().currentUser.uid);
-  // get user entered values
-  var vomiting = document.getElementById('vomiting');
-  if (vomiting.checked) {
-    symptoms.push("vomiting");
-  }
-  var muscle_spasms = document.getElementById('muscle_spasms');
-  if (muscle_spasms.checked) {
-    symptoms.push("muscle_spasms");
-  }
-  var dizziness = document.getElementById('dizziness');
-  if (dizziness.checked) {
-    symptoms.push("dizziness");
-  }
-  var fatigue = document.getElementById('fatigue');
-  if (fatigue.checked) {
-    symptoms.push("fatigue");
-  }
-  var diarrhea = document.getElementById('diarrhea');
-  if (diarrhea.checked) {
-    symptoms.push("diarrhea");
-  }
-  var skin_elasticity = document.getElementById('skin_elasticity');
-  if (skin_elasticity.checked) {
-    symptoms.push("skin_elasticity");
-  }
-  
-  // log symptoms for debugging
-  console.log(symptoms);
+  firebase.auth().onAuthStateChanged(user => {
+    // Check if user is signed in:
+    if (user) {
+      console.log('User is signed in');
+      var symptoms = [];
+      // get user entered values
+      var vomiting = document.getElementById('vomiting');
+      if (vomiting.checked) {
+        symptoms.push("vomiting");
+      }
+      var muscle_spasms = document.getElementById('muscle_spasms');
+      if (muscle_spasms.checked) {
+        symptoms.push("muscle_spasms");
+      }
+      var dizziness = document.getElementById('dizziness');
+      if (dizziness.checked) {
+        symptoms.push("dizziness");
+      }
+      var fatigue = document.getElementById('fatigue');
+      if (fatigue.checked) {
+        symptoms.push("fatigue");
+      }
+      var diarrhea = document.getElementById('diarrhea');
+      if (diarrhea.checked) {
+        symptoms.push("diarrhea");
+      }
+      var skin_elasticity = document.getElementById('skin_elasticity');
+      if (skin_elasticity.checked) {
+        symptoms.push("skin_elasticity");
+      }
 
-  // update user's symptoms in database
-  currentUser.update({
-      symptoms: symptoms,
-    })
-    .then(() => {
-      console.log("Document successfully updated!");
-    })
-    .catch((error) => {
-      console.error("Error updating document: ", error);
-    });
+      // log symptoms for debugging
+      console.log(symptoms);
+
+      currentUser = db.collection("users").doc(user.uid)
+      // update user's symptoms in database
+      currentUser.update({
+          symptoms: symptoms,
+        })
+        .then(() => {
+          console.log("Document successfully updated!");
+          window.location.href = './symptom2.html';
+        })
+        .catch((error) => {
+          console.error("Error updating document: ", error);
+        });
+    } else {
+      console.log('No user is signed in');
+    }
+  });
 }
--- src/symptom2.js
+++ src/symptom2.js
@@ -1,30 +1,127 @@
-function displayHikeInformation() {
-    let ID = "Heat Stroke"
-    console.log(ID);
+// var currentUser;
+// var symptoms;
 
-    db.collection("Heat Illnesses").doc(ID).get().then(illness => {
-            hikeInfo = illness.data();
-            hikeInfo.id = illness.id
-            console.log(hikeInfo)
+// function displayHikeInformation() {
+//     firebase.auth().onAuthStateChanged(function(user) {
+//         if (user) {
+//           // User is signed in, get user ID
+//           var userID = firebase.auth().currentUser.uid;
+//           console.log(userID);
+      
+//           // Use userID to access Firestore data
+//           db.collection("users").doc(userID).get().then(user => {
+//             var userData = user.data();
+//             console.log(userData);
+      
+//             // Access symptoms data
+//             var symptoms = userData.symptoms;
+//             console.log(symptoms);
+//           });
+//         } else {
+//           // No user is signed in
+//           console.log('No user is signed in');
+//         }
+//       });
+    
 
-            dangers = hikeInfo.Dangers;
-            remedies = hikeInfo.Remedies;
-            console.log(dangers)
-            console.log(remedies)
+//       if (symptoms.includes( "skin_elasticity")) {
+//         var ID = "Dehydration"
+//       }
+//         else if (symptoms.includes("muscle_spasms") && (!symptoms.includes("vomiting") || !symptoms.includes("diarrhea") || !symptoms.includes("fatigue"))) {
+//             var ID = "Heat Cramps"
+//         }
+//         else if (symptoms.includes("muscle_spasms") && (symptoms.includes("vomiting") || symptoms.includes("diarrhea") || symptoms.includes("fatigue"))) {
+//             var ID = "Heat Exhaustion"
+//         }
+//         else {
+//             var ID = "Heat Stroke"
+//         }
 
-            document.getElementById("illness").innerHTML = hikeInfo.id;
-            dangers.forEach(danger => {
-                const bulletPoint = document.createElement("li");
-                bulletPoint.textContent = danger;
-                $("#danger").append(bulletPoint);
-            });
+//     console.log(ID);
 
-            remedies.forEach(remedy => {
-                const bulletPoint2 = document.createElement("li");
-                bulletPoint2.textContent = remedy;
-                $("#remedy").append(bulletPoint2);
-            });
-        })    
-}
+//     db.collection("Heat Illnesses").doc(ID).get().then(illness => {
+//             hikeInfo = illness.data();
+//             hikeInfo.id = illness.id
+//             console.log(hikeInfo)
 
-displayHikeInformation();
+//             dangers = hikeInfo.Dangers;
+//             remedies = hikeInfo.Remedies;
+//             console.log(dangers)
+//             console.log(remedies)
+
+//             document.getElementById("illness").innerHTML = hikeInfo.id;
+//             dangers.forEach(danger => {
+//                 const bulletPoint = document.createElement("li");
+//                 bulletPoint.textContent = danger;
+//                 $("#danger").append(bulletPoint);
+//             });
+
+//             remedies.forEach(remedy => {
+//                 const bulletPoint2 = document.createElement("li");
+//                 bulletPoint2.textContent = remedy;
+//                 $("#remedy").append(bulletPoint2);
+//             });
+//         })    
+// }
+
+// displayHikeInformation();
+
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in, get user ID
+      var userID = firebase.auth().currentUser.uid;
+      console.log(userID);
+  
+      // Use userID to access Firestore data
+      db.collection("users").doc(userID).get().then(user => {
+        var userData = user.data();
+        console.log(userData);
+  
+        // Access symptoms data
+        var symptoms = userData.symptoms;
+        console.log(symptoms);
+  
+        // Check symptoms for signs of heat illness
+        if (symptoms.includes( "skin_elasticity")) {
+          var ID = "Dehydration"
+        } else if (symptoms.includes("muscle_spasms") && (!symptoms.includes("vomiting") || !symptoms.includes("diarrhea") || !symptoms.includes("fatigue"))) {
+          var ID = "Heat Cramps"
+        } else if (symptoms.includes("muscle_spasms") && (symptoms.includes("vomiting") || symptoms.includes("diarrhea") || symptoms.includes("fatigue"))) {
+          var ID = "Heat Exhaustion"
+        } else {
+          var ID = "Heat Stroke"
+        }
+  
+        console.log(ID);
+  
+        // Retrieve heat illness data from Firestore
+        db.collection("Heat Illnesses").doc(ID).get().then(illness => {
+          hikeInfo = illness.data();
+          hikeInfo.id = illness.id;
+          console.log(hikeInfo);
+  
+          dangers = hikeInfo.Dangers;
+          remedies = hikeInfo.Remedies;
+          console.log(dangers);
+          console.log(remedies);
+  
+          document.getElementById("illness").innerHTML = hikeInfo.id;
+          dangers.forEach(danger => {
+            const bulletPoint = document.createElement("li");
+            bulletPoint.textContent = danger;
+            $("#danger").append(bulletPoint);
+          });
+  
+          remedies.forEach(remedy => {
+            const bulletPoint2 = document.createElement("li");
+            bulletPoint2.textContent = remedy;
+            $("#remedy").append(bulletPoint2);
+          });
+        });
+      });
+    } else {
+      // No user is signed in
+      console.log('No user is signed in');
+    }
+  });
+  
