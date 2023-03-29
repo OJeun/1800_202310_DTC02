d02a166026570b921a54b7c03cc2e2072aaf3e33
MARTIN SIU <msiu28@my.bcit.ca>
2023-03-29 13:54:33 -0700

    made user profile page

--- src/userProfile.js
+++ src/userProfile.js
@@ -1,59 +1,66 @@
-var currentUser;          //put this right after you start script tag before writing any functions.
+var currentUser; //put this right after you start script tag before writing any functions.
 
 function populateUserInfo() {
-            firebase.auth().onAuthStateChanged(user => {
-                // Check if user is signed in:
-                if (user) {
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if user is signed in:
+        if (user) {
 
-                    //go to the correct user document by referencing to the user uid
-                    currentUser = db.collection("users").doc(user.uid)
-                    //get the document for current user.
-                    currentUser.get()
-                        .then(userDoc => {
-                            //get the data fields of the user
-                            var userName = userDoc.data().name;
-                            var userSchool = userDoc.data().school;
-                            var userCity = userDoc.data().city;
+            //go to the correct user document by referencing to the user uid
+            currentUser = db.collection("users").doc(user.uid)
+            //get the document for current user.
+            currentUser.get()
+                .then(userDoc => {
+                    //get the data fields of the user
+                    var userName = userDoc.data().name;
+                    var userSchool = userDoc.data().school;
+                    var userCountry = userDoc.data().country;
+                    var userCity = userDoc.data().city;
 
-                            //if the data fields are not empty, then write them in to the form.
-                            if (userName != null) {
-                                document.getElementById("nameInput").value = userName;
-                            }
-                            if (userSchool != null) {
-                                document.getElementById("schoolInput").value = userSchool;
-                            }
-                            if (userCity != null) {
-                                document.getElementById("cityInput").value = userCity;
-                            }
-                        })
-                } else {
-                    // No user is signed in.
-                    console.log ("No user is signed in");
-                }
-            });
+                    //if the data fields are not empty, then write them in to the form.
+                    if (userName != null) {
+                        document.getElementById("nameInput").value = userName;
+                    }
+                    if (userSchool != null) {
+                        document.getElementById("schoolInput").value = userSchool;
+                    }
+                    if (userCountry != null) {
+                        document.getElementById("countryInput").value = userCountry;
+                    }
+                    if (userCity != null) {
+                        document.getElementById("cityInput").value = userCity;
+                    }
+                })
+        } else {
+            // No user is signed in.
+            console.log("No user is signed in");
         }
+    });
+}
+populateUserInfo();
 
-        function addDog() {
+function editUserInfo() {
+    //Enable the form fields
+    document.getElementById('personalInfoFields').disabled = false;
+}
 
-            console.log('hello')
-            const dogTakeName = document.getElementById("nameInput");
-            const dogName = nameInput.value;
-            const dogTakeAge = document.getElementById("ageInput");
-            const dogAge = ageInput.value;
-            const dogTakeBreed = document.getElementById("breedInput");
-            const dogBreed = breedInput.value;
-            const dogTakeHair = document.getElementById("hairInput");
-            const dogHair = hairInput.value;
-        
-            user = firebase.auth().currentUser;
-            var dogsCollection = db.collection("users").doc(user.uid).collection("dogs")
-            dogsCollection.add({
-                name: dogName,
-                age: dogAge,
-                hair: dogHair,
-                breed: dogBreed
-            }).then((docRef) => {
-                console.log("Document written with ID: ", docRef.id);
-            }).catch((error) => {
-                console.log("Error adding document: ", error);
-            });
+function addDog() {
+
+    console.log('hello')
+    var userName = document.getElementById('nameInput').value; //get the value of the field with id="nameInput"
+    var userSchool = document.getElementById('schoolInput').value; //get the value of the field with id="schoolInput"
+    var userCountry = document.getElementById('countryInput').value;
+    var userCity = document.getElementById('cityInput').value;
+
+    currentUser.update({
+            name: userName,
+            school: userSchool,
+            country: userCountry,
+            city: userCity
+        })
+        .then(() => {
+            console.log("Document successfully updated!");
+        })
+
+    //c) disable edit 
+    document.getElementById('personalInfoFields').disabled = true;
+}
