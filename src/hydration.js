f9223bc4805b4422eaccf57469aa2b9f7b518a0f
2-Chengs <ivan.cheng011099@gmail.com>
2023-04-01 15:31:47 -0700

    added error message if hydration fields are empty

--- src/hydration.js
+++ src/hydration.js
@@ -6,94 +6,150 @@ var reminderCard;
 var activeButton;
 var cancelButton;
 var countdown;
-var endTIme;
-//var currentDogId = localStorage.setItem("currentDogId", currentDogId)
+var endTime;
 
 function timer() {
+    console.log("hello world");
+    // Get the current dog ID
+    var currentDogId = localStorage.getItem("currentDogId");
+    // Use the current dog ID to create unique keys for the countdown end time and timer ID
+    var countdownEndTimeKey = "countdownEndTime_" + currentDogId;
+    var timerIdKey = "timerId_" + currentDogId;
+
     reminderCard = document.getElementById("reminder");
-    var activeButton = document.getElementById("submitButton")
-    var cancelButton = document.getElementById("cancelButton");
-    var countdown = document.getElementById("countdown");
+    activeButton = document.getElementById("submitButton");
+    cancelButton = document.getElementById("cancelButton");
+    countdown = document.getElementById("countdown");
     var hours = document.getElementById("hours");
     var hoursValue = parseInt(hours.value);
-    var currentDogId = localStorage.getItem("currentDogId");
-    var countdownEndTimeKey = "countdownEndTime_" + currentDogId
-    
+
     var minutes = document.getElementById("minutes");
     var minutesValue = parseInt(minutes.value);
-
-
-    var timeInMilliSeconds = hoursValue * 3600 * 1000 + minutesValue * 60 * 1000
-   // var endTime = new Date().getTime() + timeInMilliSeconds;
-
     
-  
+    //Warns user they cannot proceed if both fields are empty
+    if (isNaN(hoursValue) && isNaN(minutesValue)) {
+        console.log("hours and minutes is empty")
+        let warning = document.createElement('span');
+        warning.textContent = "both time fields cannot be empty";
+        warning.style.color = "red";
+        var caption = document.getElementById('caption');
+
+        while(caption.firstChild) {
+            caption.removeChild(caption.firstChild);
+        }
+
+        caption.appendChild(warning);
+        return false;
+    }
+    //Warns user they cannot proceed if minutes field is empty
+    if (isNaN(minutesValue)) {
+        console.log("minutes is empty")
+        let warning = document.createElement('span');
+        warning.textContent = "minutes field cannot be empty";
+        warning.style.color = "red";
+        var caption = document.getElementById('caption');
+
+        while(caption.firstChild) {
+            caption.removeChild(caption.firstChild);
+        }
+
+        caption.appendChild(warning);
+        return false;
+    } 
     
+    //Warns user they cannot proceed if hours field is empty
+    if (isNaN(hoursValue)) {
+        console.log("hours is empty")
+        let warning = document.createElement('span');
+        warning.textContent = "hours field cannot be empty";
+        warning.style.color = "red";
+        var caption = document.getElementById('caption');
+
+        while(caption.firstChild) {
+            caption.removeChild(caption.firstChild);
+        }
+
+        caption.appendChild(warning);
+        return false;
+    }
+
+
+    var timeInMilliSeconds = hoursValue * 3600 * 1000 + minutesValue * 60 * 1000;
+    var endTime = localStorage.getItem(countdownEndTimeKey);
+
+    if (endTime) {
+        // If a countdown end time is stored, use it instead of calculating a new one
+        endTime = parseInt(endTime);
+    } else {
+        // Calculate the end time if a countdown end time is not already stored
+        endTime = new Date().getTime() + timeInMilliSeconds;
+        // Store the end time in localStorage
+        localStorage.setItem(countdownEndTimeKey, endTime);
+    }
+
     reminderCard.style.display = "none";
     activeButton.style.display = "none";
     countdown.style.display = "block";
     cancelButton.style.display = "block";
 
-    var endTime = localStorage.getItem("countdownEndTime");
-  
-    if (endTime) {
-      // If a countdown end time is stored, use it instead of calculating a new one
-      endTime = parseInt(endTime);
-      console.log("hello")
-    } else {
-      // Calculate the end time if a countdown end time is not already stored
-      endTime = new Date().getTime() + hoursValue * 60 * 60 * 1000 + minutesValue * 60 * 1000;
-      
-      // Store the end time in localStorage
-      localStorage.setItem("countdownEndTime", endTime);
-      console.log("bye")
-    }
-    
-    
     timerId = setInterval(() => {
         var now = new Date().getTime();
         var timerCountdown = endTime - now;
         hoursRemaining = Math.floor(timerCountdown / (1000 * 60 * 60));
         minutesRemaining = Math.floor((timerCountdown % (1000 * 60 * 60)) / (1000 * 60));
         secondsRemaining = Math.floor((timerCountdown % (1000 * 60)) / 1000);
-        console.log(timerCountdown);
-
 
         if (timerCountdown < 0) {
             clearInterval(timerId);
-            countdown.textContent = "Time to Drink Water!"
+            countdown.textContent = "Time to Drink Water!";
             alert("TIME TO DRINK WATER");
             // Remove the countdown end time from localStorage when the countdown is finished
-            localStorage.removeItem("countdownEndTime");
+            localStorage.removeItem(countdownEndTimeKey);
+            localStorage.removeItem(timerIdKey);
         } else {
-            //Update the countdown text
+            // Update the countdown text
             countdown.textContent = `Drink water in ${hoursRemaining} hours, ${minutesRemaining} minutes, and ${secondsRemaining} seconds`;
-            
-        }      
+        }
     }, 1000);
+
+    // Store the timer ID in local storage using the timer ID key
+    localStorage.setItem(timerIdKey, timerId);
 }
 
 window.onload = function() {
-    endTime = localStorage.getItem("countdownEndTime");
+    // Get the current dog ID
+    var currentDogId = localStorage.getItem("currentDogId");
+    // Use the current dog ID to create unique keys for the countdown end time and timer ID
+    var countdownEndTimeKey = "countdownEndTime_" + currentDogId;
+    var timerIdKey = "timerId_" + currentDogId;
+
     reminderCard = document.getElementById("reminder");
     countdown = document.getElementById("countdown");
+
+    var endTime = localStorage.getItem(countdownEndTimeKey);
     if (endTime) {
-      // If a countdown end time is stored, start the timer
-      reminderCard.style.display = "none";
-      countdown.style.display = "block";
-      timer();
+        // If a countdown end time is stored, start the timer and hide the reminder form
+        reminderCard.style.display = "none";
+        countdown.style.display = "block";
+        timer();
     } else {
+        // If no countdown end time is stored, show the reminder form
         reminderCard.style.display = "block";
         countdown.style.display = "none";
     }
 }
 
-
 function cancelTimer() {
-    var reminderCard = document.getElementById("reminder");
-    var activeButton = document.getElementById("submitButton")
-    var cancelButton = document.getElementById("cancelButton");
-    var countdown = document.getElementById("countdown");
+    // Get the current dog ID
+    var currentDogId = localStorage.getItem("currentDogId");
+    // Use the current dog ID to create unique keys for the countdown end time and timer ID
+    var countdownEndTimeKey = "countdownEndTime_" + currentDogId;
+    var timerIdKey = "timerId_" + currentDogId;
+
+    reminderCard = document.getElementById("reminder");
+    activeButton = document.getElementById("submitButton");
+    cancelButton = document.getElementById("cancelButton");
+    countdown = document.getElementById("countdown");
     hoursRemaining = 0;
     minutesRemaining = 0;
     secondsRemaining = 0;
@@ -103,8 +159,7 @@ function cancelTimer() {
     cancelButton.style.display = "none";
     countdown.style.display = "none";
     clearInterval(timerId);
-    localStorage.removeItem("countdownEndTime");
-
+    localStorage.removeItem(countdownEndTimeKey);
+    localStorage.removeItem(timerIdKey);
 }
-
 
