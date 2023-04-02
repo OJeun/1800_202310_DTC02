12203dbf792a50501327f5b14c23ad41d0ed9105
2-Chengs <ivan.cheng011099@gmail.com>
2023-04-01 20:46:26 -0700

    fixed photo upload so null photos display default snoopy img

--- src/dogProfile.js
+++ src/dogProfile.js
@@ -1,62 +1,87 @@
-var ImageFile
-    //global variable to store the File Object reference
+var ImageFile;
 
+//global variable to store the File Object reference
 function chooseFileListener() {
-  const fileInput = document.getElementById("mypic-input");   // pointer #1
-  const image = document.getElementById("mypic-goes-here");   // pointer #2
+  const fileInput = document.getElementById("mypic-input"); // pointer #1
+  const image = document.getElementById("mypic-goes-here"); // pointer #2
 
   //attach listener to input file
   //when this file changes, do something
-  fileInput.addEventListener('change', function (e) {
-
+  fileInput.addEventListener("change", function (e) {
     //the change event returns a file "e.target.files[0]"
     ImageFile = e.target.files[0];
     var blob = URL.createObjectURL(ImageFile);
 
     //change the DOM img element source to point to this file
-    image.src = blob;    //assign the "src" property of the "img" tag
-  })
+    image.src = blob; //assign the "src" property of the "img" tag
+  });
 }
 chooseFileListener();
 
 function addDog(event) {
-  event.preventDefault()
+  event.preventDefault();
   const unique_id = Math.random().toString(36).substr(2, 9);
-  
+
   const dogName = nameInput.value;
   const dogAge = ageInput.value;
   const dogBreed = breedInput.value;
   const dogHair = hairInput.value;
 
-
   user = firebase.auth().currentUser;
-  console.log(user.uid)
-  var storageRef = storage.ref("images/" + user.uid + "/" + unique_id + ".jpg");
-  var dogsCollection = db.collection("users").doc(user.uid).collection("dogs")
-  storageRef.put(ImageFile).then(function () {
-    storageRef.getDownloadURL()
-      .then(function (url) { // Get "url" of the uploaded file
-        console.log("Got the download URL.");
-        //get values from the from
-        dogsCollection.add({
-          name: dogName,
-          age: dogAge,
-          hair: dogHair,
-          breed: dogBreed,
-          profilePic: url
-        }).then(function () {
-          console.log("Hi");
-          document.getElementById("dogProfileForm").submit();
-          alert("Dog added successfully!");
-          window.location.href = "main.html";
-          // document.getElementById('personalInfoFields').disabled = true;
-        })
-
-        //Asynch call to save the form fields into Firestore.
-
+  console.log(user.uid);
+  var storageRef = storage.ref(
+    "images/" + user.uid + "/" + unique_id + ".jpg"
+  );
+  var dogsCollection = db
+    .collection("users")
+    .doc(user.uid)
+    .collection("dogs");
+  if (ImageFile) {
+    storageRef
+      .put(ImageFile)
+      .then(function () {
+        storageRef.getDownloadURL().then(function (url) {
+          // Get "url" of the uploaded file
+          console.log("Got the download URL.");
+          dogsCollection
+            .add({
+              name: dogName,
+              age: dogAge,
+              hair: dogHair,
+              breed: dogBreed,
+              profilePic: url,
+            })
+            .then(function () {
+              console.log("Hi");
+              document.getElementById("dogProfileForm").submit();
+              alert("Dog added successfully!");
+              window.location.href = "main.html";
+              // document.getElementById('personalInfoFields').disabled = true;
+            });
+        });
       })
-  })
+      .catch(function (error) {
+        console.log(error);
+        alert("Error occurred during image upload.");
+      });
+  } else {
+    dogsCollection
+      .add({
+        name: dogName,
+        age: dogAge,
+        hair: dogHair,
+        breed: dogBreed,
+        profilePic: null,
+      })
+      .then(function () {
+        console.log("Hi");
+        document.getElementById("dogProfileForm").submit();
+        alert("Dog added successfully!");
+        window.location.href = "main.html";
+      });
+  }
 }
+
 
 function populateInfo() {
   firebase.auth().onAuthStateChanged(user => {
