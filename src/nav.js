fe1365bf9074878df4fe198c43af49cec1eb9250
MARTIN SIU <msiu28@my.bcit.ca>
2023-04-02 16:25:27 -0700

    implement edit dog function into dropdown menu

--- src/nav.js
+++ src/nav.js
@@ -1,68 +1,67 @@
-
 var dogsArray = []
 // user = firebase.auth().currentUser;
 // var dogsCollection = db.collection("users").doc(user.uid).collection("dogs")
 function getName() {
-    var user = firebase.auth().currentUser;
-    var dogsCollection = db.collection("users").doc(user.uid).collection("dogs");
+  var user = firebase.auth().currentUser;
+  var dogsCollection = db.collection("users").doc(user.uid).collection("dogs");
 
-    dogsCollection.get().then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-            // Access the fields in the document using the data() method
-            const dogData = doc.data();
-            const dogId = doc.id;
+  dogsCollection.get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      // Access the fields in the document using the data() method
+      const dogData = doc.data();
+      const dogId = doc.id;
 
-            dogsArray.push({
-                id: dogId,
-                name: dogData.name,
-                age: dogData.age,
-                breed: dogData.breed
-            })
+      dogsArray.push({
+        id: dogId,
+        name: dogData.name,
+        age: dogData.age,
+        breed: dogData.breed
+      })
 
-        });
-    }).catch((error) => {        
-        console.log("Error getting dogs collection:", error);
     });
-    console.log(dogsArray);
+  }).catch((error) => {
+    console.log("Error getting dogs collection:", error);
+  });
+  console.log(dogsArray);
 
 }
 
 function logout() {
-    firebase.auth().signOut().then(function() {
-      // Sign-out successful.
-      window.location.href = "index.html";
-      console.log("User signed out successfully.");
-  
-      // Clear local storage data
-      localStorage.removeItem("currentDogName");
-      localStorage.removeItem("currentDogId");
-      localStorage.removeItem("username")
-  
-  
-      // Redirect to the login page
-      window.location.href = "index.html";
-    }).catch(function(error) {
-      // An error happened.
-      console.error("Error signing out: ", error);
-    });
-  }
-  
-  var formModal = document.getElementById("formModal");
-  var showFormLink = document.getElementById("showFormLink");
-  var closeBtn = document.getElementsByClassName("close")[0];
-  
-  showFormLink.onclick = function() {
-    var user = firebase.auth().currentUser;
-    formModal.style.display = "block";
-    var editNameInput = document.getElementById("editNameInput");
-    var editEmailInput = document.getElementById("editEmailInput");
-    var editSchoolInput = document.getElementById("editSchoolInput");
-    db.collection("users").doc(user.uid).get().then(doc => {
-        editNameInput.value = doc.data().name;
-        editEmailInput.value = doc.data().email;
-        editSchoolInput.value = doc.data().school;
-    })
-  }
+  firebase.auth().signOut().then(function () {
+    // Sign-out successful.
+    window.location.href = "index.html";
+    console.log("User signed out successfully.");
+
+    // Clear local storage data
+    localStorage.removeItem("currentDogName");
+    localStorage.removeItem("currentDogId");
+    localStorage.removeItem("username")
+
+
+    // Redirect to the login page
+    window.location.href = "index.html";
+  }).catch(function (error) {
+    // An error happened.
+    console.error("Error signing out: ", error);
+  });
+}
+
+var formModal = document.getElementById("formModal");
+var showFormLink = document.getElementById("showFormLink");
+var closeBtn = document.getElementsByClassName("close")[0];
+
+showFormLink.onclick = function () {
+  var user = firebase.auth().currentUser;
+  formModal.style.display = "block";
+  var editNameInput = document.getElementById("editNameInput");
+  var editEmailInput = document.getElementById("editEmailInput");
+  var editSchoolInput = document.getElementById("editSchoolInput");
+  db.collection("users").doc(user.uid).get().then(doc => {
+    editNameInput.value = doc.data().name;
+    editEmailInput.value = doc.data().email;
+    editSchoolInput.value = doc.data().school;
+  })
+}
 
 
 
@@ -78,15 +77,15 @@ updateProfileForm.addEventListener('submit', (e) => {
   const school = editSchoolInput.value;
 
   const user = firebase.auth().currentUser;
-  
+
   user.updateEmail(email)
     .then(() => {
       // Email updated successfully
       console.log("Email updated successfully");
 
       db.collection("users").doc(user.uid).set({
-        name: name, 
-        school: school, 
+        name: name,
+        school: school,
         email: email
       }).then(() => {
         alert("Profile updated successfully");
@@ -100,16 +99,16 @@ updateProfileForm.addEventListener('submit', (e) => {
       console.log("Error updating email:", error);
     });
 });
-  
-  closeBtn.onclick = function() {
+
+closeBtn.onclick = function () {
+  formModal.style.display = "none";
+}
+
+window.onclick = function (event) {
+  if (event.target == formModal) {
     formModal.style.display = "none";
   }
-  
-  window.onclick = function(event) {
-    if (event.target == formModal) {
-      formModal.style.display = "none";
-    }
-  }
+}
 
 // // // Get the user's name from the database
 
@@ -150,13 +149,13 @@ updateProfileForm.addEventListener('submit', (e) => {
 //     firebase.auth().signOut().then(function() {
 //       // Sign-out successful.
 //       console.log("User signed out successfully.");
-  
+
 //       // Clear local storage data
 //       localStorage.removeItem("currentDogName");
 //       localStorage.removeItem("currentDogId");
 //       localStorage.removeItem("username")
-  
-  
+
+
 //       // Redirect to the login page
 //       window.location.href = "login.html";
 //     }).catch(function(error) {
@@ -164,6 +163,26 @@ updateProfileForm.addEventListener('submit', (e) => {
 //       console.error("Error signing out: ", error);
 //     });
 //   }
-  
 
-  
+firebase.auth().onAuthStateChanged(function (user) {
+  currentUser = db.collection("users").doc(user.uid).collection("dogs");
+  var dropdown = document.getElementById("dropdown-menu");
+  currentUser.get().then((querySnapshot) => {
+    if (querySnapshot.size === 0) {
+      var defaultOption = document.createElement("option");
+      defaultOption.text = "No dogs added";
+      dropdown.appendChild(defaultOption);
+    } else {
+      querySnapshot.forEach((doc) => {
+        var option = document.createElement("option");
+        option.text = doc.data().name;
+        option.onclick = function() {
+          var url = "addDog.html"
+          window.location.href = url;
+        }
+        dropdown.appendChild(option);
+      });
+    }
+  });
+});
+
